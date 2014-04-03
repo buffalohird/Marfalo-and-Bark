70eb5ff7582f8f9451e86e9495d21fcff1392456
Buffalo Hird <buffalohird@college.harvard.edu>
2014-04-03 17:11:56 -0400

    pushing

--- hw6-code-2014/cs186-mturk-intro.js
+++ hw6-code-2014/cs186-mturk-intro.js
@@ -1,14 +1,16 @@
 // Problem 1.  Warming up
 
 // TO-DO:  Find the functions to print your balance and the # of open hits.
+var hits = mturk.getHITs()
 print("Hello MTurk!")
-print("Your balance is: " + <YOUR CODE>  )
-print("No. of hits: "+ <YOUR CODE> )
+print("Your balance is: "+ mturk.getAccountBalance() )
+print("No. of hits: "+ hits.length)
+
 
 // Explore the HIT object. 
 // Get the first HIT and then iterate over its properties.
 if(mturk.getHITs().length>0) {
-	var hitObj = <YOUR CODE> 
+	var hitObj = hits[0]
 	for(attr in hitObj) 
 		print("Attr ="+attr+ " val="+hitObj[attr])
 }
